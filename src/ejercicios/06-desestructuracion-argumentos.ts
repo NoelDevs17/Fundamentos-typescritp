2fe0053bf243cfa4fe85eaa4f23771e96e42b89e
Noel Ortiz <20175580@itla.edu.do>
2021-09-09 00:45:43 -0400

    desestructuracion  de argumentos

---
+++ src/ejercicios/06-desestructuracion-argumentos.ts
@@ -0,0 +1,38 @@
+//desestructuracion de argumentos
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto  =  {
+    desc: 'Iphone XS MAX',
+    precio: 400
+}
+
+const tableta: Producto = {
+    desc: 'Samsung',
+    precio: 170
+}
+
+
+
+
+
+function calculaISV( productos: Producto[] ): [number, number] {
+
+    let total = 0;
+
+    productos.forEach( ( {precio})  => {
+        total += precio;
+    })
+
+    return [total,total * 0.15];
+}
+
+const articulos = [telefono , tableta];
+
+const[ total, isv ] =calculaISV(articulos);
+
+console.log('total: ',total );
+console.log('ISV: ', isv);
